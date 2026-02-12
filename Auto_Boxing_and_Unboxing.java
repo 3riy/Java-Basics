58c3fa7b76d348a4664e73e7c57bab5087a5555a
Riya halbhavi <halbhaviriya@gmail.com>
2026-02-12 12:47:33 +0530

    Update Auto_Boxing_and_Unboxing.java

--- Auto_Boxing_and_Unboxing.java
+++ Auto_Boxing_and_Unboxing.java
@@ -3,14 +3,18 @@
 class Auto_Boxing_and_Unboxing {
     public static void main(String args[]) {
         int num = 2;
-        Integer num1 = num; // Auto-boxing
-        System.out.println("Auto-boxing: " + num1); // Output: 2
 
-        int num2 = num1; // Auto-unboxing
-        System.out.println("Auto-unboxing: " + num2); // Output: 2
+        // Auto-boxing: Converting primitive int to Integer object
+        Integer num1 = num;
+        System.out.println("Auto-boxing: " + num1); /* Output: 2 */
+
+        // Auto-unboxing: Converting Integer object back to primitive int
+        int num2 = num1;
+        System.out.println("Auto-unboxing: " + num2); /* Output: 2 */
         
         String str = "11";
-        int num3 = Integer.parseInt(str); // Converting String to int
-        System.out.println("String to int: " + num3); // Output: 11
+        // Auto-unboxing with parsing: Converting String to int
+        int num3 = Integer.parseInt(str);
+        System.out.println("String to int: " + num3); /* Output: 11 */
     }
 }
